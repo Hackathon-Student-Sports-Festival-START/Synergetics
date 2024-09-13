cc48034311469bbc53dbfac6ad2b99b15417a3fb
Zhorantiy <geo.proleev@gmail.com>
2024-09-13 20:01:07 +0300

    login

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./admin'));
+router.use(require('./scheduleRoutes'));
+router.use(require('./login'));
+
 
 // router.get('/login', (req, res) => {
 //     res.render('pages/admin/login');
