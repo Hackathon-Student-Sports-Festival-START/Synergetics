f5ccb73cdd1452acb718d7d2700cc9787f66fa3c
nick-755 <ValentinValeryevich@yandex.ru>
2024-09-13 21:24:34 +0300

    all change

--- config/database.js
+++ config/database.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('database', 'username', 'lsndvkuhi723fy283vcni3j8fc23iuosm', {
   host: '213.32.103.188',
-  port: '15023',
+  port: 15032,
   dialect: 'postgres'
 });
 
 module.exports = sequelize;
+
+
+
+
+
